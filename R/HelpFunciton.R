e115c9b1f8ca5a14a644950b285813bac556e206
ziyue-li317 <imliziyue@gmail.com>
2024-11-27 02:16:11 -0500

    start functions

---
+++ R/HelpFunciton.R
@@ -0,0 +1,33 @@
+# library(devtools)
+# library(knitr)
+# library(roxygen2)
+# library(testthat)
+#  devtools::has_devel()
+# usethis::use_mit_license("Ziyue Li")
+# usethis::use_package("mclust", type = "Imports")
+# usethis::use_readme_md()
+# usethis::use_readme_rmd()
+
+# devtools::load_all()
+# devtools::document()
+
+
+# usethis::use_git()
+
+# library(gitcreds)
+
+# link local repository with Github
+# slide 56-57 of L4
+
+
+
+
+
+
+
+
+
+
+
+
+
