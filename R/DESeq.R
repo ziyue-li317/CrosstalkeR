2730faa1b65187c1a4e9857bab2048aa86046ff9
ziyue-li317 <imliziyue@gmail.com>
2024-11-27 03:52:22 -0500

    copy data process from previous scripts which lack some requiered content

---
+++ R/DESeq.R
@@ -0,0 +1,124 @@
+# here we use DESeq to screen genes from example data
+
+
+# load packages
+library(edgeR)
+library(limma)
+library(Glimma)
+library(org.Mm.eg.db)
+library(gplots)
+library(RColorBrewer)
+library(NMF)
+library(dplyr)
+library(DESeq2)
+
+# data preparation
+mycounts_unf <-my_data
+
+
+# filtering lowly expressed genes
+cpm_value <- cpm(mycounts_unf)
+keep_genes <- rowSums(cpm_value >=0.5) >= 7
+mycounts <- mycounts_unf[keep_genes, ]
+
+# condition
+condition <- factor(c(rep("lactate_control",2), rep("lactate_treat",2),
+                      rep("pregnant_control",2), rep("pregnant_treat", 2),
+                      rep("virgin_control", 2), rep("virgin_treat", 2)),
+                    level = c("lactate_control", "lactate_treat",
+                              "pregnant_control", "pregnant_treat",
+                              "virgin_control", "virgin_treat"))
+
+# colData
+colData <- data.frame(row.names = colnames(mycounts), condition)
+colData
+
+# create dds
+dds <- DESeqDataSetFromMatrix(mycounts, colData, design = ~ condition)
+# dds normalization
+dds_norm <- DESeq(dds)
+# gain data after normalization
+normalized_counts <- counts(dds_norm, normalized = TRUE)
+head(normalized_counts)
+
+# order data by difference in expression : mad value
+normalized_counts_mad <- apply(normalized_counts, 1, mad)
+normalized_counts <- normalized_counts[order(normalized_counts_mad,
+                                             decreasing = T), ]
+
+# log
+rld <- rlog(dds_norm, blind = FALSE)
+rlogMat <- assay(rld)
+rlogMat <- rlogMat[order(normalized_counts_mad, decreasing = T), ]
+
+# data output after normalization
+write.table(normalized_counts, file = "dds_normalized_counts.xls",
+            quote = F, sep = "\t", row.names = T, col.names = T)
+write.table(rlogMat, file = "dds_normalized_counts_rlog.xls",
+            quote = F, sep = "\t", row.names = T, col.names = T)
+
+
+# analyse differences in expression
+# lactate
+res_lactate <- results(dds_norm, contrast = c("condition",
+                                              "lactate_control", "lactate_treat" ))
+res_lactate = res_lactate[order(res_lactate$pvalue), ]
+head(res_lactate)
+summary(res_lactate)
+write.csv(res_lactate, file = "lactate_result.csv")
+
+diff_gene_deseq2_lactate <- subset(res_lactate,padj < 0.05 &
+                                     (log2FoldChange >1 | log2FoldChange < -1))
+dim(diff_gene_deseq2_lactate) # 6321*6
+head(diff_gene_deseq2_lactate)
+write.csv(diff_gene_deseq2_lactate, file = "diff_gene_deseq2_lactate.csv")
+
+#annotation
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("biomaRt")
+library("biomaRt")
+library("curl")
+
+mart <- useDataset("mmusculus_gene_ensembl", useMart("ensembl"))
+entrez_ids <- row.names(diff_gene_deseq2_lactate)
+
+my_gene_ids <- getBM(
+  attributes = c("entrezgene_id", "ensembl_gene_id"),
+  filters = "entrezgene_id",
+  values = entrez_ids,
+  mart = mart
+)
+
+head(my_gene_ids)
+my_ensembl_gene_id <- my_gene_ids[, 2]
+
+hg_symbols <-getBM(
+  attributes = c('ensembl_gene_id', 'external_gene_name',"description" ),
+  filters = "ensembl_gene_id",
+  values = my_ensembl_gene_id,
+  mart = mart
+)
+head(hg_symbols)
+
+
+all_symbols <-merge(hg_symbols, my_gene_ids, by="ensembl_gene_id", all = FALSE)
+
+
+entrezgene_id <- rownames(diff_gene_deseq2_lactate)
+diff_gene_deseq2_lactate <-cbind(entrezgene_id, diff_gene_deseq2_lactate)
+colnames(diff_gene_deseq2_lactate)[1] <- c("entrezgene_id")
+diff_name <- merge(diff_gene_deseq2_lactate, all_symbols, by="entrezgene_id",
+                   all = FALSE)
+head(diff_gene_deseq2_lactate)
+head(diff_name)
+
+
+
+
+
+
+
+
+
---
+++ R/data.R
@@ -0,0 +1,79 @@
+
+
+#install basic packages
+if (!requireNamespace("BiocManager"))
+  install.packages("BiocManager")
+BiocManager::install(c("limma", "edgeR", "Glimma", "org.Mm.eg.db",
+                       "gplots", "RColorBrewer", "NMF", "BiasedUrn"))
+library(edgeR)
+library(limma)
+library(Glimma)
+library(org.Mm.eg.db)
+library(gplots)
+library(RColorBrewer)
+library(NMF)
+library(dplyr)
+
+install.packages("dplyr", dependencies = TRUE)
+library(dplyr)
+
+#data input and arrange
+input_data_un <-read.delim("data/GSE60450_Lactation-GenewiseCounts.txt",
+                           stringsAsFactors = FALSE)
+sampleinfo_un <- read.delim("data/SampleInfo.txt", stringsAsFactors = TRUE)
+rownames(sampleinfo_un) <-sampleinfo_un$FileName
+sampleinfo <- sampleinfo_un[, -1]
+
+
+transposed_data <- t(input_data_un)
+transposed_data <- as.data.frame(transposed_data)
+
+dim(sampleinfo_un) # 12 4
+dim(transposed_data) # 14 27179
+
+fixed_rows <-transposed_data[1:2, ]
+remianing_rows <- transposed_data[-(1:2), ]
+
+dim(sampleinfo) # 12 4
+dim(remianing_rows) # 12 27179
+
+combined_rows <- merge(sampleinfo, remianing_rows,
+                       by = "row.names", all = TRUE)
+dim(combined_rows) # 12 27183
+ordered_rows <-combined_rows %>% arrange(Status)
+row_to_move <- ordered_rows[9, ]
+ordered_rows <- ordered_rows[-9, ]
+ordered_rows <- rbind(ordered_rows, row_to_move)
+
+new_first_column <- c("lactate control1",
+                      "lactate control2", "lactate treat1",
+                      "lactate treat2", "pregnant control1",
+                      "pregnant control2", "pregnant treat1",
+                      "pregnant treat2", "virgin control1",
+                      "virgin control2", "virgin treat1", "virgin treat2")
+ordered_rows <-cbind(NewColumn = new_first_column, ordered_rows)
+rownames(ordered_rows) <- ordered_rows$Row.names
+ordered_rows <- ordered_rows[, -(1:2)]
+ordered_rows <-cbind(NewColumn = new_first_column, ordered_rows)
+
+rownames(ordered_rows) <- ordered_rows$NewColumn
+ordered_rows <- ordered_rows[, -(1:3)]
+ordered_rows <- ordered_rows[, -1]
+
+
+
+
+final_data <-rbind(fixed_rows, ordered_rows)
+
+
+
+
+
+
+final_data <-t(final_data)
+final_data <-as.data.frame(final_data)
+final_data <-final_data[, -(2)]
+rownames(final_data) <- final_data[, 1]
+my_data <- final_data[, -1]
+# here we generate right formal for DESeq2
+write.csv(my_data, file = "my_data.csv", row.names = TRUE)
